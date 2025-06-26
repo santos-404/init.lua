f647757d9fb53eba434c449beff1d3111d83ec17
Javier Santos <javsanmar5@alum.us.es>
2025-06-26 12:14:50 +0200

    Add exit insert mode when hitting jj

--- lua/javsanmar5/remap.lua
+++ lua/javsanmar5/remap.lua
@@ -15,3 +15,8 @@ vim.keymap.set("n", "<A-j>", ":m+1<CR>==", { noremap = true, silent = true })
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
 
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("i", "kk", "<Esc>")
+vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("i", "kj", "<Esc>")
+
