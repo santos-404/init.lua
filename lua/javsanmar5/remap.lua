de90c079e136c9aa293cfba71b1407aebe006d36
Javier Santos <javier.jsm21@gmail.com>
2026-01-16 13:38:14 +0100

    Add map to navigate errors

--- lua/javsanmar5/remap.lua
+++ lua/javsanmar5/remap.lua
@@ -15,6 +15,8 @@ vim.keymap.set("n", "<A-j>", ":m+1<CR>==", { noremap = true, silent = true })
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
 
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic" })
+
 -- vim.keymap.set("i", "jj", "<Esc>")
 -- vim.keymap.set("i", "kk", "<Esc>")
 -- vim.keymap.set("i", "jk", "<Esc>")
