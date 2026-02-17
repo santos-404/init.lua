090fd06449df65bf2d82de1984587a615398a5c4
Javier Santos <javier.jsm21@gmail.com>
2026-02-17 12:00:01 +0100

    Update live grep hotkey

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<C-S-f>', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>ff', builtin.live_grep, { desc = 'Telescope live grep' })
 
 require('telescope').setup({
   defaults = {
